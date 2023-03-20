aa287caa5c814f8b492e5591dad0136e0fabb1d0
mikemouse <kmmyemail@ukr.net>
2023-03-20 21:30:16 +0200

    feat: add carousel

--- js/main.js
+++ js/main.js
@@ -297,4 +297,84 @@ window.addEventListener('DOMContentLoaded', () => {
   fetch('http://localhost:3000/menu')
     .then((data) => data.json())
     .then((res) => console.log(res));
+
+  //CAROUSEL
+  //1. query slides, prev, next, total slides counter, current slide number, slidesWrapper-overflow, slidesInner-400%, set width as wrapper width, index = 1, offset = 0
+  //2. set styles inner = 100 * 4 '%', disp=flex, transit=0.5s all
+  //3. set wrapper overflow=hidden
+  //4. set each slide width = wrapper width
+  //5. next addEv if(offset == int(width)*sl.len-1) offset 0 else offset += int(width) translateX${offset}
+  //6. prev addEv if(offset == 0) offset int(width)*sl.len-1 else offset -= int(width) translateX${offset}
+  //7. numbers next if(slideIndex==length) 1 else ++ if(slides.length<10) 0slides.length else slides.length
+  //8. numbers prev if(slideIndex==1) slides.length else -- if(slides.length<10) 0slides.length else slides.length
+  const slides = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current'),
+    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    slidesField = document.querySelector('.offer__slider-inner'),
+    width = window.getComputedStyle(slidesWrapper).width;
+  let slideIndex = 1,
+    offset = 0;
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
+
+  slidesField.style.width = 100 * slides.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
+
+  slidesWrapper.style.overflow = 'hidden';
+
+  slides.forEach((slide) => {
+    slide.style.width = width;
+  });
+
+  next.addEventListener('click', () => {
+    if (offset == parseInt(width) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += parseInt(width);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  });
+
+  prev.addEventListener('click', () => {
+    if (offset == 0) {
+      offset = parseInt(width) * (slides.length - 1);
+    } else {
+      offset -= parseInt(width);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  });
 });
