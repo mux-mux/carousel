ff6bb2a2a949218bfc3433e12cb85024402960f3
mikemouse <kmmyemail@ukr.net>
2023-03-21 13:52:29 +0200

    feat: add nav buttons

--- js/main.js
+++ js/main.js
@@ -303,10 +303,11 @@ window.addEventListener('DOMContentLoaded', () => {
   //2. set styles inner = 100 * 4 '%', disp=flex, transit=0.5s all
   //3. set wrapper overflow=hidden
   //4. set each slide width = wrapper width
-  //5. next addEv if(offset == int(width)*sl.len-1) offset 0 else offset += int(width) translateX${offset}
-  //6. prev addEv if(offset == 0) offset int(width)*sl.len-1 else offset -= int(width) translateX${offset}
+  //5. next addEv if(offset == int(width)*sl.len-1) offset 0 else offset += int(width) transform translateX${offset}
+  //6. prev addEv if(offset == 0) offset int(width)*sl.len-1 else offset -= int(width) transform translateX${offset}
   //7. numbers next if(slideIndex==length) 1 else ++ if(slides.length<10) 0slides.length else slides.length
   //8. numbers prev if(slideIndex==1) slides.length else -- if(slides.length<10) 0slides.length else slides.length
+
   const slides = document.querySelectorAll('.offer__slide'),
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next'),
@@ -355,6 +356,8 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+    dots.forEach((dot) => dot, (style.opacity = '.5'));
+    dots[slideIndex - 1].style.opacity = 1;
   });
 
   prev.addEventListener('click', () => {
@@ -376,5 +379,86 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+    dots.forEach((dot) => dot, (style.opacity = '.5'));
+    dots[slideIndex - 1].style.opacity = 1;
+  });
+
+  //NAVS Carousel
+  //1. query whole slider not only wrapper position relative, dots []
+  //2. create indic = ol.carousel-indicators cssText abs,0,0,0,z15,df,jcc,mr15%,ml15%,lsn
+  //3. add nav wrapper slider.append(indic)
+  //4. loop make circles count of total slides craete li setAttr('data-slide-to', i+1)
+  //5. cssText bx-sz cont-box fl 0 1 auto w30 h6 mr3 ml 3 curp bgc#fff bgclip padd-box bt 10 sol trans bb 10 sol trans op .5 transit op .6s ease indic.append(dot) dots.push(dot)
+  //6. add active class if i == 0 op = 1
+  //7. click left right move slide num prev, next dots.each op .5 dots[slideIndex - 1].style.opacity = 1
+  //9. click dot move to slide num dot.each slideIndex = e get attr data-slide-to
+  //8. ^ offset int(width)*slTo-1 transform translateX${offset}
+  //9. ^ click dot move to slide num dot.each slideIndex = e get attr data-slide-to
+  //10. ^ if(slides.length<10) 0slides.length else slides.length
+
+  const slider = document.querySelector('.offer__slider');
+
+  slider.style.position = 'relative';
+
+  const indicators = document.createElement('ol'),
+    dots = [];
+
+  indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+  `;
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+      box-sizing: content-box;
+      flex: 0 1 auto;
+      width: 30px;
+      height: 6px;
+      margin-right: 3px;
+      margin-left: 3px;
+      cursor: pointer;
+      background-color: #fff;
+      background-clip: padding-box;
+      border-top: 10px solid transparent;
+      border-bottom: 10px solid transparent;
+      opacity: .5;
+      transition: opacity .6s ease;
+    `;
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
+  dots.forEach((dot) => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = parseInt(width) * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      dots.forEach((dot) => dot, (style.opacity = '.5'));
+      dots[slideIndex - 1].style.opacity = 1;
+
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = slideIndex;
+      }
+    });
   });
 });
